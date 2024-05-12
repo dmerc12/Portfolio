6093b3301aa754373678ef527bf533ddbe41ec52
dmerc12 <100182684+dmerc12@users.noreply.github.com>
2024-05-12 02:12:28 -0500

    adds particle container

--- front-end/components/Avatar.js
+++ front-end/components/Avatar.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
-      <Image src={'/my-avatar.png'} width={737} height={678} alt='' className='translate-z-0 w-full h-full'/>
+      <Image src={'/my-avatar.png'} width={737} height={678} alt='' className='translate-z-0 w-full h-full z-11'/>
     </div>
   );
 };
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,4 +1,4 @@
-import ParticclesContainer from '../components/ParticlesContainer';
+import ParticlesContainer from '../components/ParticlesContainer';
 import ProjectsBtn from '../components/ProjectsBtn';
 import Avatar from '../components/Avatar';
 import { motion } from 'framer-motion';
@@ -22,10 +22,10 @@ const Home = () => {
       </div>
       <div className='w-[1200px] h-full absolute right-0 bottom-0'>
         <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
-          <div>Particles</div>
-          <div className='w-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
+          <ParticlesContainer />
+          <motion.div variants={fadeIn('up', 0.5)} initial='hidden' animate='show' exit='hidden' transition={{ duration: 1, ease: 'easeInOut' }} className='w-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
             <Avatar />
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
